c311b916910a5b68175c586b4dde5694189de73b
Manash <manashmndl@gmail.com>
2020-04-21 10:09:35 +0600

    implementing tee

--- main.c
+++ main.c
@@ -10,4 +10,28 @@
 #endif
 #define MAX_READ 512
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  char *testfilepath = "/home/manash/KernelProgramming/helloworld.txt";
+  char buff[BUF_SIZE];
+  int fd;
+  ssize_t numRead;
+  ssize_t numWritten;
+
+  fd = open(testfilepath, O_RDWR);
+
+  if (fd == -1) {
+    errMsg("open");
+  }
+
+  while (numRead = read(STDIN_FILENO, buff, MAX_READ)) {
+    if (numRead == -1) {
+      errMsg("read");
+    }
+    buff[numRead] = '\0';
+    numWritten = write(fd, buff, numRead);
+    if (numWritten == -1) {
+      errMsg("write");
+    }
+    printf("%s", buff);
+  }
+}
